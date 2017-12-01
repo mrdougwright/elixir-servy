ea542df6473ed1e29168bd607b26441aa4eef443
Doug Wright <mrdougwright@gmail.com>
2017-11-30 22:29:44 -0800

    short hand syntax

--- lib/servy/bear_controller.ex
+++ lib/servy/bear_controller.ex
@@ -10,8 +10,8 @@ defmodule Servy.BearController do
   def index(conv) do
     items =
       Wildthings.list_bears()
-      |> Enum.filter(fn(b) -> Bear.is_grizzly(b) end)
-      |> Enum.sort(fn(b1, b2) -> Bear.order_asc_by_name(b1, b2) end)
+      |> Enum.filter(&Bear.is_grizzly/1)
+      |> Enum.sort(&Bear.order_asc_by_name/2) # shorthand
       |> Enum.map(fn(b) -> bear_item(b) end)
       |> Enum.join
 
