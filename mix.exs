ce6a5f54bfcb205138730d0522e8555c9c7ec05d
Doug Wright <mrdougwright@gmail.com>
2017-12-31 00:13:44 -0800

    add HTTPoison

--- mix.exs
+++ mix.exs
@@ -21,7 +21,8 @@ defmodule Servy.Mixfile do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:poison, "~> 3.1"} # json encoder from http://hex.pm
+      {:poison, "~> 3.1"}, # json encoder from http://hex.pm
+      {:httpoison, "~> 0.12.0"} # http client for elixir
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -25,4 +25,13 @@ defmodule Servy.HttpServerTest do
     Bears, Lions, Tigers
     """
   end
+
+  test "accepts a request on a socket and sends back a response" do
+    spawn(HttpServer, :start, [4000])
+
+    {:ok, response} = HTTPoison.get "http://localhost:4000/wildthings"
+
+    assert response.status_code == 200
+    assert response.body == "Bears, Lions, Tigers"
+  end
 end
