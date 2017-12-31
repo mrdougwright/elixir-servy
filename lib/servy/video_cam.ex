dcba5b4efc9192a39cfcbcd9e443a7b69b5b66b6
Doug Wright <mrdougwright@gmail.com>
2017-12-30 16:22:27 -0800

    sending and receiving messages

---
+++ lib/servy/video_cam.ex
@@ -0,0 +1,13 @@
+defmodule Servy.VideoCam do
+  @doc """
+  Simulates sending a request to an external API
+  to get a snapshot image from a video camera.
+  """
+  def get_snapshot(camera_name) do
+    # CODE GOES HERE TO SEND A REQUEST TO EXTERNAL API
+
+    :timer.sleep(1000)
+
+    "#{camera_name}-snapshot.jpg"
+  end
+end
